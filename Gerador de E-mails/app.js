e824a5561bc4044e284389c0ba1c15cc482fc494
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2022-09-24 11:27:37 -0300

    subindo gerador de emails

---
+++ Gerador de E-mails/app.js
@@ -0,0 +1,23 @@
+document.getElementById('btGerar').addEventListener('click', function gerarEmail (){
+    let resp = document.querySelector('div#resposta');
+    let inNome = document.getElementById('inNome');
+    let inNomeEmpresa = document.getElementById('inNomeEmpresa');
+    let nomeEmpresa = inNomeEmpresa.value;
+    let nome = inNome.value;
+
+    let nomepartes = nome.split(' ');
+    let primeiroNome = nomepartes[0];
+    let email ='';
+    let tamNome = nomepartes.length;
+    for(let i =0; i < tamNome; i++){
+        email+= nomepartes[i].charAt(0);
+    }
+    resp.innerHTML = `${primeiroNome}${email}@${nomeEmpresa}.com.br`;
+
+})
+function verificarInfo (){
+    if(x == ' '){
+        let resp = document.querySelector('div#resposta');
+        resp.innerHTML = `Prencha o campo corretamente..`;
+    }
+}
