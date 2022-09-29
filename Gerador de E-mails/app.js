d26f5f7d646046c8fbd2b61863ac0e6a2985a944
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2022-09-29 20:10:59 -0300

    comentando cód js

--- Gerador de E-mails/app.js
+++ Gerador de E-mails/app.js
@@ -4,24 +4,33 @@ document.getElementById('btGerar').addEventListener('click', function gerarEmail
     let inNomeEmpresa = document.getElementById('inNomeEmpresa');
     let nomeEmpresa = inNomeEmpresa.value;
     let nome = inNome.value;
+
+    //chamando funções
+    verificarInfo (nome, nomeEmpresa);
     estilo(resp);
+
+    //tranforma o nome completo em um vetor
     let nomepartes = nome.split(' ');
     let primeiroNome = nomepartes[0];
+     //var p concatenar
     let email ='';
     let tamNome = nomepartes.length;
     for(let i =0; i < tamNome; i++){
+         //pega a primeira letra de cada nome e sobrenome
         email+= nomepartes[i].charAt(0);
     }
     resp.innerHTML = `${primeiroNome}${email}@${nomeEmpresa}.com.br`;
 
 })
-function verificarInfo (){
-    if(x == ' '){
+ //valida entrada de dados
+function verificarInfo (x, y){
+    if(x == ' ' || y == ' '){
         let resp = document.querySelector('div#resposta');
         resp.innerHTML = `Prencha o campo corretamente..`;
     }
 }
+ //estilo da resposta
 function estilo(y){
     let word = document.getElementById('resposta');
-    word.style.backgroundColor = '#fff';
+    word.style.backgroundColor = '#dbdbdb';
 }
