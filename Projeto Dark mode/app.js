ba884ec4405077807c538d473380681d37729685
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2022-12-23 14:15:06 -0300

    excluindo dark mode

--- Projeto Dark mode/app.js
+++
@@ -1,7 +0,0 @@
-const bt = document.querySelector('.bt');
-const container = document.querySelector('.container');
-
-bt.onclick = function () {
-    this.classList.toggle('active');
-    container.classList.toggle('active');
-}
